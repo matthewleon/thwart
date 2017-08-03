7ef6ce4e344f6d014a7dff3e842614b5551f66a5
Matthew Leon <ml@matthewleon.com>
2017-08-03 18:07:59 +0100

    play algorithm
    tests if a move is legal

--- src/Main.purs
+++ src/Main.purs
@@ -10,14 +10,16 @@ import Control.Monad.ST (runST, newSTRef, readSTRef, writeSTRef)
 import Data.Array as Array
 import Data.Dictionary (Dictionary)
 import Data.Dictionary as Dictionary
+import Data.Either (Either(..))
 import Data.Foldable (any)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(..), fromJust)
+import Data.NonEmpty (NonEmpty, (:|))
 import Data.Ord (abs)
 import Data.Set (Set)
 import Data.Set as Set
-import Data.String (Pattern(Pattern))
+import Data.String (Pattern(Pattern), toCharArray)
 import Data.String as String
 import Node.Encoding (Encoding(ASCII))
 import Node.FS (FS)
@@ -95,16 +97,17 @@ main = do
       currentState' <- readSTRef currentState
       log $ show currentState'
       case turn currentState' dict s of
-        Just newState -> void $ writeSTRef currentState newState
-        Nothing -> pure unit
+        Right newState -> void $ writeSTRef currentState newState
+        otherwise -> log "word not in dictionary"
       prompt interface
 
 -- TODO: monadify
-turn :: GameState -> Dictionary -> String -> Maybe GameState
+data TurnError = TurnWordNotInDictionary | TurnWordUnplayable
+turn :: GameState -> Dictionary -> String -> Either TurnError GameState
 turn (GameState gs) dict str
   | dict `Dictionary.has` str =
     let playedWords' = Set.insert gs.previousWord gs.playedWords
-    in  Just <<< GameState $ case gs.currentPlayer of
+    in  Right <<< GameState $ case gs.currentPlayer of
           Player1 -> {
             playedWords: playedWords'
           , previousWord: str
@@ -119,9 +122,36 @@ turn (GameState gs) dict str
           , score2: gs.score2 + movePoints str gs.previousWord gs.playedWords
           , currentPlayer: Player1
           }
-  | otherwise = Nothing
+  | otherwise = Left TurnWordNotInDictionary
 
+
+data Play = AddedLetters (NonEmpty Array Char)
+          | RemovedLetters (NonEmpty Array Char)
+          | Pass -- TODO: should this be legal?
+
+play :: String -> String -> Maybe Play
+play oldWord newWord =
+  case compare (String.length oldWord) (String.length newWord) of
+    EQ -> if oldWord == newWord then Just Pass else Nothing
+    LT -> AddedLetters <$> play' oldWord newWord
+    GT -> RemovedLetters <$> play' newWord oldWord
+  where
+  play' :: String -> String -> Maybe (NonEmpty Array Char)
+  play' shortWord longWord =
+    (Array.uncons $ go (toCharArray shortWord) (toCharArray longWord))
+      <#> \{head: c, tail: cs'} -> c :| cs'
+    where
+    go :: Array Char -> Array Char -> Array Char
+    go scs lcs = case Array.uncons scs of
+      Just {head: c, tail: cs} -> case Array.elemIndex c lcs of
+        Just i  -> go cs $ Array.drop (i + 1) lcs
+        Nothing -> lcs
+      Nothing -> lcs
+
+
+-- TODO: should be able to just count array size from playableWord
 movePoints :: String -> String -> Set String -> Int
 movePoints newWord lastWord playedWords
   | Set.member newWord playedWords = 0
   | otherwise = abs $ String.length newWord - String.length lastWord
+
